2928201745c71c14d9c977e9439f8a15819c37f8
Erol KOCAMAN <kocamanerol@gmail.com>
2014-01-24 10:55:55 +0200

    Dead Ghost animation

--- core/src/main/java/net/javaci/mobile/bomberman/core/GameFactory.java
+++ core/src/main/java/net/javaci/mobile/bomberman/core/GameFactory.java
@@ -15,7 +15,7 @@ public class GameFactory {
     static {
         GameModel level1 = new GameModel();
         level1.numGhosts = 10;
-        level1.numBricks = 20;
+        level1.numBricks = 10;
         level1.numBomb = 2;
         gameModels.put(1, level1);
     }
